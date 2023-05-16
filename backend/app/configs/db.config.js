e4d5307e9ae2bd2e6324914266ea7a8827d0625d
JOAO PAULO FAUST <joao.faust@alunos.sc.senac.br>
2023-05-16 11:37:35 -0300

    Projeto lanchonete

---
+++ backend/app/configs/db.config.js
@@ -0,0 +1,10 @@
+//Arquivo de configurações do banco de dados
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "xdev",
+    PORT: "3306"
+}
+
+
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+
+app.unsubscribe(express.urlencoded({
+    extended:true
+}));
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Bem vindo a API MVC do Senac"
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+})
+
